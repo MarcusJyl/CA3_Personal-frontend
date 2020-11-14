36324add92bee6ed74a381edba80179a2be4a1bc
Marcus Ravn Jensen <marcusravn1996'gmail.com>
2020-11-14 23:44:17 +0100

    first commit

---
+++ frontend/src/facades/settings.js
@@ -0,0 +1,4 @@
+export const loginURL = "http://localhost:8080/jpareststarter";
+
+export const jokeURL = "http://localhost:8080/jpareststarter/api/jokes";
+export const KanyeURL = "http://localhost:8080/jpareststarter/api/Kanye";
