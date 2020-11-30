83157b30ea810fba35436447ab6d05b27f6906f8
Marcus Ravn Jensen <marcusravn1996'gmail.com>
2020-11-30 10:02:02 +0100

    fix

--- frontend/src/facades/settings.js
+++ frontend/src/facades/settings.js
@@ -1,5 +1,5 @@
-export const loginURL = "https://marcusravnjensen.dk/CA3_Personal";
-export const allNamesURL = "https://marcusravnjensen.dk/CA3_Personal/api/user/info";
+export const loginURL = "http://localhost:8080/jpareststarter";
+export const allNamesURL = "http://localhost:8080/jpareststarter/api/user/info";
 
-export const jokeURL = "https://marcusravnjensen.dk/CA3_Personal/api/jokes";
-export const KanyeURL = "https://marcusravnjensen.dk/CA3_Personal/api/Kanye";
+export const jokeURL = "http://localhost:8080/jpareststarter/api/jokes";
+export const KanyeURL = "http://localhost:8080/jpareststarter/api/Kanye";
